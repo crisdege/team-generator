3b7d980a602212fcf5c4f66694776e9678632520
Cristina Dege <cris.dege0813@gmail.com>
2022-08-08 10:11:23 -0400

    engineer constructor done

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,11 @@
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+
+test("creates a manager object", () => {
+  const engineer = new Engineer("Jared", 1, "me@me.com", "jared");
+
+  expect(engineer.name).toBe("Jared");
+  expect(engineer.id).toEqual(expect.any(Number));
+  expect(engineer.email).toEqual(expect.any(String));
+  expect(engineer.github).toEqual(expect.any(String));
+});
